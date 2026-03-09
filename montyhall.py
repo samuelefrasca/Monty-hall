0195e682f3f79b9f17c3a5cdfaac2deeecfb3a5a
Samuele Frasca <samufra06@gmail.com>
2026-03-09 17:00:08 +0100

    Add file in GitHub

---
+++ montyhall.py
@@ -0,0 +1,71 @@
+# PROBLEMA DI MONTY HALL
+
+import random
+import time
+
+def montyhall(num):
+    if not isinstance(num, int):
+        raise TypeError("Il numero di porte deve essere un numero intero!")
+    if num<3:
+        raise ValueError("Il gioco richiede almeno 3 porte!")
+    # POSIZIONE DELL'AUTO
+    doors = [False] * num # False indica le capre, True indica l'auto
+    car = random.randint(0,num-1)
+    doors[car] = True
+    # Inzio del gioco
+    n = int(input(f"PROBLEMA DI MONTIHALL!\nScegli un numero tra 1 e {num}: "))
+    if doors[n-1]==False:
+        if num == 3:
+            print("Ok, adesso aprirò una porta in cui so che c'è una capra...")
+        else:
+            print("Ok, adesso aprirò delle porte in cui so che ci sono delle capre...")
+        time.sleep(3)
+        for i in range(len(doors)):
+            if i != (n-1) and not doors[i]:
+                print(f"Nella porta {i+1} c'è una capra!")
+        print(f"Rimangono chiuse la porta {n} (ossia quella scelta da te) e la porta {car+1}")
+        while True:
+            question = int(input("Vuoi cambiare la tua scelta? (digita 0 per NON cambiare, oppure 1 per cambiare): "))
+            if question==0 or question==1:
+                if question==0:
+                    print(f"Ok, quindi hai deciso di restare sulla porta {n}, vediamo la soluzione...")
+                    time.sleep(3)
+                    print(f"Hai perso! Mi dispiace, la macchina era nella porta {car+1}")
+                    return
+                if question==1:
+                    print(f"Ok, quindi hai deciso di cambiare la porta, scegliendo la {car+1} al posto della {n}, vediamo la soluzione...")
+                    time.sleep(3)
+                    print(f"Congratulazioni, hai vinto! la macchina era proprio nella porta {car+1}!")
+                    return
+            else:
+                print("Devi scegliere un numero tra 0 e 1")
+    if doors[n-1]:
+        nocar = random.randint(0,num-1)
+        while nocar == n-1: # Il ciclo while serve ad evitare che nocar sia uguale a n
+            nocar = random.randint(0,num-1)
+        if num == 3:
+            print("Ok, adesso aprirò una porta in cui so che c'è una capra...")
+        else:
+            print("Ok, adesso aprirò delle porte in cui so che ci sono delle capre...")
+        time.sleep(3)
+        for i in range(len(doors)):
+            if i != (n-1) and i != nocar:
+                print(f"Nella porta {i+1} c'è una capra!")
+        print(f"Rimangono chiuse la porta {nocar+1} (ossia quella scelta da te) e la porta {car+1}")
+        while True:
+            question = int(input("Vuoi cambiare la tua scelta? (digita 0 per NON cambiare, oppure 1 per cambiare)"))
+            if question==0 or question==1:
+                if question==0:
+                    print(f"Ok, quindi hai deciso di restare sulla porta {car+1}, vediamo la soluzione...")
+                    time.sleep(3)
+                    print(f"Congratulazioni, hai vinto! la macchina era proprio nella porta {car+1}!")
+                    return
+                if question==1:
+                    print(f"Ok, quindi hai deciso di cambiare la porta, scegliendo la {nocar+1} al posto della {car+1}, vediamo la soluzione...")
+                    time.sleep(3)
+                    print(f"Hai perso! Mi dispiace, la macchina era nella porta {car+1}")
+                    return
+            else:
+                print("Devi scegliere un numero tra 0 e 1")
+
+montyhall(3)
